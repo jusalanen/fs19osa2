1e54ed70a7cf6f785ef109b86ca895167a615737
jusalanen <jussialanen9@gmail.com>
2019-07-19 17:36:06 +0300

    lisää tehtävä 2.5

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -1,54 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.course}</h2>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = ( {parts} ) => {
-  console.log(parts)
-  return <div>
-  {parts.map( part =>
-    <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-  </div>
-}
-
-const Total = ( {parts} ) => {
-  const reducer = (total, part) => total + part.exercises
-  let tot = parts.reduce(reducer, 0)
-  return (
-    <div>
-      <b>total of {tot} exercises</b>
-    </div>
-  )
-}
-
-const Course = (props) => {
-  console.log(props)
-  const { course } = props
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts ={course.parts} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const Curriculum = ( {courses} ) => {
   return (
@@ -110,7 +62,6 @@ const App = () => {
       <Curriculum courses = {courses} />
     </div>
   )
-
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
