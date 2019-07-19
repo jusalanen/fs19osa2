42eca5a2ed138955c444d2dd45008078d1451e6e
jusalanen <jussialanen9@gmail.com>
2019-07-19 19:46:49 +0300

    fix 2.6

---
+++ puhelinluettelo/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const Phonebook = ( {persons} ) => {
+    return <div>
+      {persons.map( person => <div>
+      {person.name} </div>)}
+    </div>
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName} 
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Phonebook persons={persons} />
+    </div>
+  )
+
+}
+
+export default App
